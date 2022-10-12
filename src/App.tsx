52a5a9fc5284f11c89844a7d6b34ad6a011b66c4
zjh1687 <zjh1687@naver.com>
2022-10-12 23:57:49 +0900

    scss 기본세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import AnalogClock from './analogClock/AnalogClock';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import './styles/index.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root'));
