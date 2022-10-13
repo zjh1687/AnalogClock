d95a426885c534c85b4d9d9bf33be319596178e0
zjh1687 <zjh1687@naver.com>
2022-10-14 00:40:59 +0900

    툽팁 기능 추가

--- src/analogClock/AnalogClock.tsx
+++ src/analogClock/AnalogClock.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useTimeDispatch, useTimeSelector } from 'src/store/hook';
 import { setHour, setMinute, setSecond } from 'src/store/reducer';
 import ClockHand from './components/ClockHand';
 import HourNumber from './components/HourNumber';
 
 function AnalogClock() {
+  const rootRef = useRef<HTMLDivElement>(null);
+  const tooltipRef = useRef<HTMLDivElement>(null);
   const dispatch = useTimeDispatch();
   const { hour, second, minute } = useTimeSelector((state) => state);
 
@@ -16,11 +18,28 @@ function AnalogClock() {
     dispatch(setSecond(time.getSeconds()));
   }, 1000);
 
+  const handleTooltip = (e: MouseEvent) => {
+    console.log(e.clientX, rootRef.current!.clientWidth);
+    tooltipRef.current!.style.left = e.offsetX - 20 + 'px';
+    tooltipRef.current!.style.top = e.offsetY - 60 + 'px';
+  };
+
+  useEffect(() => {
+    rootRef.current?.addEventListener('mousemove', (e) => {
+      handleTooltip(e);
+    });
+    return () => {
+      rootRef.current?.removeEventListener('mousemove', (e) => {
+        handleTooltip(e);
+      });
+    };
+  }, []);
+
   const clockNumberArr = Array.from({ length: 12 }, (v, i) => i);
 
   return (
     <div className="clock-wrapper">
-      <div className="clock-circle">
+      <div className="clock-circle" ref={rootRef}>
         <div className="hour">
           {clockNumberArr.map((i) => {
             return <HourNumber key={`hour+${i}`} number={i + 1} />;
@@ -37,6 +56,9 @@ function AnalogClock() {
         <ClockHand color="black" width="8px" height="40%" deg={minute * 6} />
         {/* 초침 */}
         <ClockHand color="red" width="4px" height="35%" deg={second * 6} />
+        <div className="tooltip" ref={tooltipRef}>
+          현재시각:{`${hour}시 ${minute}분 ${second}초`}
+        </div>
       </div>
     </div>
   );
--- src/analogClock/components/HourNumber.tsx
+++ src/analogClock/components/HourNumber.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function HourNumber(props: { number: number }) {
   return (
-    <div
+    <span
       className="hour-number"
       style={{ transform: `rotate(${props.number * 30}deg)` }}
     >
       <p style={{ transform: `rotate(-${props.number * 30}deg)` }}>
         {props.number}
       </p>
-    </div>
+    </span>
   );
 }
 
