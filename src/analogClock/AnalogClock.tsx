3fea8c2ca80d9963ff3d167e6741b7286a46e23a
zjh1687 <zjh1687@naver.com>
2022-10-13 21:15:45 +0900

    시간 적용, 시계침 계산식 추가

--- src/analogClock/AnalogClock.tsx
+++ src/analogClock/AnalogClock.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ClockHand from './components/ClockHand';
+import HourNumber from './components/HourNumber';
 
 function AnalogClock() {
-  return <div></div>;
+  const [hourDeg, setHoutDeg] = useState<number>(0);
+  const [minuteDeg, setMinuteDeg] = useState<number>(0);
+  const [secondDeg, setSecondDegg] = useState<number>(0);
+  const clockNumberArr = Array.from({ length: 12 }, (v, i) => i);
+
+  setInterval(() => {
+    const time = new Date();
+    setHoutDeg(time.getHours() * 30);
+    setMinuteDeg(360 - time.getMinutes());
+    setSecondDegg(time.getSeconds() * 6);
+  }, 1000);
+
+  return (
+    <div className="clock-wrapper">
+      <div className="clock-circle">
+        <div className="hour">
+          {clockNumberArr.map((i) => {
+            return <HourNumber key={`hour+${i}`} number={i + 1} />;
+          })}
+        </div>
+        {/* 시침 */}
+        <ClockHand color="black" width="8px" height="20%" deg={hourDeg} />
+        {/* 분침 */}
+        <ClockHand color="black" width="8px" height="40%" deg={minuteDeg} />
+        {/* 초침 */}
+        <ClockHand color="red" width="4px" height="35%" deg={secondDeg} />
+      </div>
+    </div>
+  );
 }
 
 export default AnalogClock;
