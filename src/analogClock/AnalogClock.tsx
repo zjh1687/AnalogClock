deeeec6bc52c562238a7b2778e3ff9ed2ef748a2
zjh1687 <zjh1687@naver.com>
2022-10-13 22:30:12 +0900

    시간값 store 설정, 시간에 따른 시계침 각도 설정

--- src/analogClock/AnalogClock.tsx
+++ src/analogClock/AnalogClock.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useTimeDispatch, useTimeSelector } from 'src/store/hook';
+import { setHour, setMinute, setSecond } from 'src/store/reducer';
 import ClockHand from './components/ClockHand';
 import HourNumber from './components/HourNumber';
 
 function AnalogClock() {
-  const [hourDeg, setHoutDeg] = useState<number>(0);
-  const [minuteDeg, setMinuteDeg] = useState<number>(0);
-  const [secondDeg, setSecondDegg] = useState<number>(0);
-  const clockNumberArr = Array.from({ length: 12 }, (v, i) => i);
+  const dispatch = useTimeDispatch();
+  const { hour, second, minute } = useTimeSelector((state) => state);
 
+  //현재 시간 상태 store 등록
   setInterval(() => {
     const time = new Date();
-    setHoutDeg(time.getHours() * 30);
-    setMinuteDeg(360 - time.getMinutes());
-    setSecondDegg(time.getSeconds() * 6);
+    dispatch(setHour(time.getHours()));
+    dispatch(setMinute(time.getMinutes()));
+    dispatch(setSecond(time.getSeconds()));
   }, 1000);
+
+  const clockNumberArr = Array.from({ length: 12 }, (v, i) => i);
 
   return (
     <div className="clock-wrapper">
@@ -24,11 +27,11 @@ function AnalogClock() {
           })}
         </div>
         {/* 시침 */}
-        <ClockHand color="black" width="8px" height="20%" deg={hourDeg} />
+        <ClockHand color="black" width="8px" height="20%" deg={hour * 30} />
         {/* 분침 */}
-        <ClockHand color="black" width="8px" height="40%" deg={minuteDeg} />
+        <ClockHand color="black" width="8px" height="40%" deg={360 - minute} />
         {/* 초침 */}
-        <ClockHand color="red" width="4px" height="35%" deg={secondDeg} />
+        <ClockHand color="red" width="4px" height="35%" deg={second * 6} />
       </div>
     </div>
   );
