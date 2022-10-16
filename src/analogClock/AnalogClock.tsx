d1f5a1b337967ad18c206fb721fd9ce70d0a503a
zjh1687 <zjh1687@naver.com>
2022-10-16 22:30:26 +0900

    컴포넌트 변경에 따른 코드 수정

--- src/analogClock/AnalogClock.tsx
+++ src/analogClock/AnalogClock.tsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ClockTooltip from 'src/components/feature/ClockTooltip';
-import { useAppDispatch, useAppSelector } from 'src/store/hook';
+import { useAppDispatch } from 'src/store/hook';
 import ClockHand from './components/ClockHand';
 import HourNumber from './components/HourNumber';
 
 import { setTime } from './slices/timeSlice';
 import { setTooltipPosition } from './slices/tootipSlice';
+import { throttle } from 'lodash';
 
 function AnalogClock() {
   const rootRef = useRef<HTMLDivElement>(null);
   const dispatch = useAppDispatch();
-  const { hour, second, minute } = useAppSelector((state) => state.time);
+  // 툴팁 상태 관리
+  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
 
-  const handleTooltip = (e: MouseEvent) => {
+  // 실시간 마우스 좌표 없데이트
+  // Thrttle 설정을 통해 일정 시간후 함수가 동작하도록 설정
+  const handleTooltip = throttle((e: MouseEvent) => {
     dispatch(setTooltipPosition({ position: { x: e.clientX, y: e.clientY } }));
-  };
+  }, 10);
 
   //현재 시간 상태 store 등록
   useEffect(() => {
@@ -22,42 +26,44 @@ function AnalogClock() {
       const time = new Date();
       dispatch(setTime(time));
     }, 1000);
-
+    // CleanUp 작업
     return () => clearInterval(interval);
   }, []);
 
+  // mouse 움직임에 반응하는 이벤트 리스너 등록
   useEffect(() => {
     rootRef.current?.addEventListener('mousemove', (e) => {
       handleTooltip(e);
     });
+    // CleanUp 작업
     return () => {
       rootRef.current?.removeEventListener('mousemove', (e) => {
         handleTooltip(e);
       });
     };
   }, []);
-
+  //12시 까지 표시하기위한 arr 생성
   const clockNumberArr = Array.from({ length: 12 }, (v, i) => i);
 
   return (
     <div className="clock-wrapper">
-      <div className="clock-circle" ref={rootRef}>
+      <div
+        className="clock-circle"
+        ref={rootRef}
+        onMouseEnter={() => {
+          setTooltipOpen(true);
+        }}
+        onMouseLeave={() => {
+          setTooltipOpen(false);
+        }}
+      >
         {clockNumberArr.map((i) => {
           return <HourNumber key={`hour+${i}`} number={i + 1} />;
         })}
-
-        {/* 시침 */}
-        <ClockHand
-          color="black"
-          width="8px"
-          height="20%"
-          deg={hour * 30 + (minute * 6) / 12}
-        />
-        {/* 분침 */}
-        <ClockHand color="black" width="8px" height="40%" deg={minute * 6} />
-        {/* 초침 */}
-        <ClockHand color="red" width="4px" height="35%" deg={second * 6} />
-        <ClockTooltip />
+        {/* 시계침 컴포넌트 */}
+        <ClockHand />
+        {/* 움직이는 툴팁 컴포넌트 */}
+        <ClockTooltip open={tooltipOpen} />
       </div>
     </div>
   );
