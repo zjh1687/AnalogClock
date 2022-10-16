addcdec15b87d73ec1df644b9ff0df2c9a983207
zjh1687 <zjh1687@naver.com>
2022-10-16 22:30:14 +0900

    툴팁 컴포넌트 구조 변경

--- src/components/FollowTooltip.tsx
+++ src/components/FollowTooltip.tsx
@@ -4,12 +4,17 @@ function FollowTooltip(props: {
   x: number;
   y: number;
   children: React.ReactNode;
+  open?: boolean;
 }) {
   return (
     // 툴팁이 우상단에 위치할 수 있는 좌표값
     <div
       className="tooltip"
-      style={{ left: `${props.x}px`, top: `${props.y - 30}px` }}
+      style={{
+        left: `${props.x}px`,
+        top: `${props.y - 30}px`,
+        visibility: props.open ? 'visible' : 'hidden',
+      }}
     >
       {props.children}
     </div>
--- src/components/feature/ClockTooltip.tsx
+++ src/components/feature/ClockTooltip.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import FollowTooltip from 'src/components/FollowTooltip';
 import { useAppSelector } from 'src/store/hook';
 
-function ClockTooltip() {
+function ClockTooltip(props: { open?: boolean }) {
   const { hour, minute, second } = useAppSelector((state) => state.time);
   const { x, y } = useAppSelector((state) => state.tooltlp.position);
 
   return (
-    <FollowTooltip x={x} y={y}>
+    <FollowTooltip x={x} y={y} open={props.open}>
       현재시각:{`${hour}시 ${minute}분 ${second}초`}
     </FollowTooltip>
   );
